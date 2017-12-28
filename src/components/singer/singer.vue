e28e16d599c90585029b0082585e4301ee818426
ysm <ysm@ysmdeMacBook-Pro.local>
2017-12-28 16:55:32 +0800

    singer-normalizesinger

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,6 +7,9 @@
 <script type="text/ecmascript-6">
 	import {getSingerList} from 'api/singer'
 	import {ERR_OK} from 'api/config'
+	import Singer from 'common/js/singer'
+	const HOT_NAME = '热'
+	const HOT_SINGER_LEN = 10
   export default {
   	data() {
   		return {
@@ -21,12 +24,54 @@
   			getSingerList().then((res) => {
   				if(res.code === ERR_OK){
   					this.singers = res.data.list
-  					console.log(this.singers)
+  					console.log(this._normalizeSinger(this.singers))
   				}
   			})
+  		},
+  		_normalizeSinger(list) {
+  			let map = {
+  				hot: {
+  					title: HOT_NAME,
+  					items: []
+  				}
+  			}
+  			list.forEach((item,index) => {
+  				if (index < HOT_SINGER_LEN) {
+  					map.hot.items.push(new Singer({
+  						id: item.Fsinger_mid,
+  						name: item.Fsinger_name
+  					}))
+  				}
+					const key = item.Findex
+					if (!map[key]) {
+						map[key] = {
+							title: key,
+							items: []
+						}
+					}
+					map[key].items.push(new Singer({
+						id: item.Fsinger_mid,
+						name: item.Fsinger_name
+					}))
+				})
+				console.log(map)
+				// 处理为有序列表
+				let hot = []
+				let ret = []
+				for(let key in map) {
+					let val = map[key]
+					if (val.title.match(/[a-zA-Z]/)) {
+						ret.push(val)
+					} else if (val.title === HOT_NAME) {
+						hot.push(val)
+					}
+				}
+				ret.sort((a,b) => {
+					return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+				})
+				return hot.concat(ret)
   		}
   	}
-
   }
 </script>
 
