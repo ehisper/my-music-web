f8509b2b7d6713311f956c2bf907eb12a504bd20
ysm <ysm@ysmdeMacBook-Pro.local>
2018-04-19 19:30:37 +0800

    start dev search

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box" />
+    <i class="icon-dismiss"></i>
+  </div>
+</template>
+<script>
+  export default {
+    name:,
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    watch: {
+
+    }
+  }
+</script>
+<style>
+</style>
