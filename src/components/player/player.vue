0c9331d8e5e5ba3abc039bb9d404993eeeb82fdc
ysm <ysm@ysmdeMacBook-Pro.local>
2018-05-08 14:14:17 +0800

    11-1 add playlist

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -100,8 +100,8 @@
         @error="error"
         @timeupdate="updateTime"
         @ended="end"></audio>
-      <!-- <playlist ref="playlist"></playlist>
-      <audio ref="audio" :src="currentSong.url" @play="ready" @error="error" @timeupdate="updateTime"
+      <playlist ref="playlist"></playlist>
+      <!-- <audio ref="audio" :src="currentSong.url" @play="ready" @error="error" @timeupdate="updateTime"
              @ended="end"></audio> -->
     </div>
   </div>
@@ -117,6 +117,7 @@ import {playMode} from 'common/js/config'
 import {shuffle} from 'common/js/util'
 import Lyric from 'lyric-parser'
 import Scroll from 'base/scroll/scroll'
+import Playlist from 'components/playlist/playlist'
 
 const transform = prefixStyle('transform')
 const transitionDuration = prefixStyle('transitionDuration')
@@ -425,7 +426,8 @@ export default {
   components: {
     ProgressBar,
     ProgressCircle,
-    Scroll
+    Scroll,
+    Playlist
   },
   watch: {
     currentSong(newSong, oldSong) {
