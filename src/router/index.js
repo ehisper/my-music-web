4554e4b484492625ecac92522ce85e4a2a83648d
ysm <ysm@ysmdeMacBook-Pro.local>
2018-05-11 16:41:46 +0800

    12 user-center start

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Tab from 'components/tab/tab'
 import SingerDetail from 'components/singer-detail/singer-detail'
 import Disc from 'components/disc/disc'
 import TopList from 'components/top-list/top-list'
+import UserCenter from 'components/user-center/user-center'
 Vue.use(Router)
 
 export default new Router({
@@ -59,6 +60,10 @@ export default new Router({
     {
       path: '/tab',
       component: Tab
-    }
+    },
+    {
+      path: '/user',
+      component: UserCenter
+    },
   ]
 })
